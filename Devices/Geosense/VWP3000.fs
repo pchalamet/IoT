44bcc92b0b125e74d7b0e04baf3a75a0e71930e2
Pierre Chalamet <pierre@chalamet.net>
2020-01-26 21:23:14 +0100

    fix conversion function

--- Devices/Geosense/VWP3000.fs
+++ Devices/Geosense/VWP3000.fs
@@ -1,18 +1,17 @@
 ﻿module VWP3000
+open System
 
 type Configuration =
     { A : decimal
       B : decimal
-      C : decimal
-      Th : decimal
-      T0 : decimal }
+      Th : decimal }
 
 
-let compute (config : Configuration) (pressureMilliAmps : decimal) (temperatureMilliAmps : decimal) =
-    let currentPressureHz = ((3500m - 1850m) / (20m - 4m)) * (pressureMilliAmps - 4m) + 1850m
+let compute (config : Configuration) (pressureMilliAmps : decimal) (temperatureMilliAmps : decimal) (P0 : decimal) (T0 : decimal) =
+    let C = -config.A * P0 * P0 - config.B * P0
+    let pressureHz = ((3500m - 1850m) / (20m - 4m)) * (pressureMilliAmps - 4m) + 1850m
+    let digit = (pressureHz * pressureHz) / 1000m
     let currentTemperature = ((80m + 20m) / (20m - 4m)) * (temperatureMilliAmps- 4m) - 20m
 
-    let currentPressure = config.A * currentPressureHz*currentPressureHz + config.B * currentPressureHz + config.C + config.Th * (currentTemperature - config.T0)
+    let currentPressure = config.A * digit * digit + config.B * digit + C + config.Th * (currentTemperature - T0)
     currentPressure
-
-
