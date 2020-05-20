f08a4b660b01184500333509e631dbf70830aabc
Pierre Chalamet <pierre@chalamet.net>
2020-05-20 15:08:08 +0200

    build exe

--- Decoder/Program.fs
+++ Decoder/Program.fs
@@ -11,32 +11,10 @@ open Devices.MCF88.MCFLW
 let usage() =
     printfn "Decoder <payload>"
 
-let serialize o =
-    let options = JsonSerializerOptions()
-    options.WriteIndented <- true
-    options.Converters.Add(JsonFSharpConverter())
-    JsonSerializer.Serialize(o, options)
-
-
-let decode s =
-    let msg = Devices.MCF88.MCFLW.DecodeUplinkMessage s
-    printfn "%A" msg
-    printfn "%s" (msg |> serialize)
-
 [<EntryPoint>]
 let main argv =
-    let content = System.IO.File.ReadAllLines(argv.[0])
-    for s in content do 
-        let msg = Devices.MCF88.MCFLW.DecodeUplinkMessage s
-        match msg with 
-        | UplinkMessage.AnalogData m -> printfn "%A;%A" m.Timestamp m.Battery
-        | _ -> printfn "Unknown data"
-
-
-    //for s in argv do
-    //    printfn ">>> %s" s
-    //    decode s
-
+    for payload in argv do
+        let msg = Devices.MCF88.MCFLW.DecodeUplinkMessage payload
+        printfn "%s" payload
+        printfn "%A" msg
     0
-
-
--- Makefile
+++ Makefile
@@ -2,6 +2,7 @@ config ?= Debug
 
 build:
 	dotnet build -c $(config)
+	dotnet publish -o out/ Decoder/Decoder.fsproj
 
 test:
 	dotnet test -c $(config)
